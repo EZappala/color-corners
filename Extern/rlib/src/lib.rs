695f2bb515139a51819fe6e73b452ca0f63b6361
Éloïse Zappala <eloise.zappala@proton.me>
2025-10-17 12:20:35 -0400

    feat: update gitignore for rlib

--- Extern/rlib/src/lib.rs
+++ Extern/rlib/src/lib.rs
@@ -1,3 +1,58 @@
-fn main() {
-    println!("Hello, world!");
+use runity::{DataStruct, Quaternion, String, Vector3};
+
+// This function is called when the script is first run
+#[unsafe(no_mangle)]
+pub extern "C" fn awake(mut _data: DataStruct) -> DataStruct {
+    _data
+}
+
+// This function is called after awake, use it to initialize values and setup the rest of the script
+#[unsafe(no_mangle)]
+pub extern "C" fn start(mut data: DataStruct) -> DataStruct {
+    data.transform.position =
+        Vector3::translate(data.transform.position, Vector3::new(0.0, 5.0, 0.0));
+    data
+}
+
+// This function is run every frame
+#[unsafe(no_mangle)]
+pub extern "C" fn update(mut data: DataStruct) -> DataStruct {
+    let time = data.time; // Extract time related things.
+
+    let tag = String::from("Player"); // Get the tag for "Player". Make sure to free this string once you're done with it (before you return).
+
+    let player_obj = data.game_object.get_gameobject_from_tag(&tag); // Get the game object associated with the tag
+
+    let pos_to_go_towards = player_obj.transform.position;
+
+    data.transform.position = Vector3::lerp(
+        data.transform.position,
+        pos_to_go_towards,
+        0.0015 * time.delta_time,
+    ); // move towards the player using lerp, scaling to the timestep
+
+    data.transform.rotation *= Quaternion::from_euler(
+        5.0 * time.delta_time,
+        5.0 * time.delta_time,
+        5.0 * time.delta_time,
+    );
+
+    data.debug.log("Hello from rust!".to_string());
+
+    tag.free(); // free the tag
+
+    data
+}
+
+// This function is called when the script is destroyed
+#[unsafe(no_mangle)]
+pub extern "C" fn destroy(data: DataStruct) -> i32 {
+    // This function is called when the script is destroyed. This MUST be called, to ensure that the memory is freed.
+
+    // This is a good place to free any memory that was allocated in the awake function. For example, if you allocated a string, you should free it here.
+
+    // For example, the tag of the game object is a string, so we need to free it.
+    data.game_object.tag.free();
+
+    0
 }
